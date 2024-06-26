943b1fb3099d8fa8c171ec1a6f912eb8c97dd1d9
Areeba naz farooqui <areebanaz4567@gmail.com>
2024-06-26 15:50:31 +0500

    head tail game

---
+++ app.js
@@ -0,0 +1,81 @@
+// var array1 = [1,2,3]
+// var array2 = array1
+// array1 . push (4)
+// console.log (array1)
+// console.log (array2)
+
+// var myname = "Areeba";
+// myname. toLowerCase ()
+// var loweredstring = myname.toUpperCase ()
+// console.log (loweredstring);
+// var studentname = "Mahnoor hina noor";
+// var array1 = studentname.split(" ");
+// for(var i = 0; i< array1.length; i++){
+//     console.log( array1[i][0].toUpperCase()+array1[i].slice(1).toLowerCase())
+// }
+// var text = "hello my name is areeba";
+// var textlength = text.length
+// for(var i = 0; i < text.length; i++){
+//     console.log(text[i]);
+// }
+// console.log(text.slice(i,i+2));
+// console.log(text.slice(1,3));
+
+// var firstletter = "Areeba" =studentnames[0].touppercase();
+// console.log(firstletter);
+// var resetletter = studentnames.slice(1).tolowercase()
+// console.log(resetletter);
+// var result = firstletter+resetletter
+// console.log(result);
+
+
+// var userMonth = prompt("enter your month")
+
+// var userMonthLength = userMonth.length;
+
+// if(userMonthLength>3){
+//     console.log(userMonth.slice(0,3));
+
+    
+// }
+
+
+
+
+// var firstLetter = studentName[0].toUpperCase();
+//  console.log(firstLetter);
+
+// var restLetters = studentName.slice(1).toLowerCase()
+// console.log(restLetters);
+
+// var result = firstLetter+restLetters
+// console.log(result);
+
+// var result = studentName[0].toUpperCase()+ studentName.slice(1).toLowerCase()
+// console.log(result);
+
+// var num1   = 2;
+// var num2 = num1;
+// num1 = num1+5
+// console.log(num1);
+// console.log(num2);
+
+var player1 = prompt("Enter head name");
+var player2 = prompt("Enter tail name");
+var toss = Math.random ( )*2
+var change = Math.floor( toss )
+if (change == 0){
+    alert(player1 + "head win");   
+}   
+else{
+    alert(player2 + "tail win");
+}
+// var studentName =("areeba") 
+// console.log (studentName.charAt("5"));
+
+// var userpasssword = prompt("Enter your password");
+// var passwordlength = +prompt("Enter the password");
+// for(var i = 1; i <=passwordlength; i++){
+//     console.log(userpasssword);
+// }
+
